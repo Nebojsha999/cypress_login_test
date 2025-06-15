bb68802467da1e3e25aabb5f8b096f4a9aad02fc
Nebojsha999 <nebojshaangelov@gmail.com>
2025-06-15 17:56:58 +0200

    Login as standard user and add item to cart

--- cypress/e2e/tests/allTests.cy.js
+++ cypress/e2e/tests/allTests.cy.js
@@ -2,9 +2,18 @@
 import { loginPage } from "./loginPage";
 const lp = new loginPage();
 
+import { globalLocators } from "./globalLocators";
+const gl = new globalLocators();
+
 it ('Login as standard user', ()=>{
   lp.loginUser("standard_user","secret_sauce")
   cy.contains('Swag Labs').should('exist')
   cy.url().should('eq', 'https://www.saucedemo.com/inventory.html')
+});
+it ('Login as standard user and add item to cart', ()=>{
+  lp.loginUser("standard_user","secret_sauce")
+  cy.get(gl.addToCartSauceLabsBacpak).click();
+  cy.get(gl.removeSauceLabBacpak).should('be.visible')
+  cy.get(gl.shoppingCartBadge).should('contain', '1')
 });
 
--- cypress/e2e/tests/globalLocators.js
+++ cypress/e2e/tests/globalLocators.js
@@ -4,5 +4,9 @@ export class globalLocators {
     username='#user-name';
     password='#password';
     loginButton='#login-button';
+    addToCartSauceLabsBacpak='#add-to-cart-sauce-labs-backpack';
+    removeSauceLabBacpak='#remove-sauce-labs-backpack';
+    shoppingCartBadge = 'span[data-test="shopping-cart-badge"]';
+
 
 }
